a6fa2b2847951f5e1a9f774439b4d3c6e3c41233
Rafael Custodio Cumpri <rafaelcumpri.senai564@gmail.com>
2024-05-08 17:00:04 -0300

    feat : getHeroByParam

--- src/controllers/heroes.controllers.js
+++ src/controllers/heroes.controllers.js
@@ -12,3 +12,25 @@ async function getAllHeros(req, res) {
         res.json({ error: error.message });
     }
 }
+
+async function getHeroByParam(req, res) {
+    const { param } = req.params;
+    try {
+        if (isNaN(param)) {
+            const result = await pool.query('SELECT * FROM herois WHERE nome Like $1', [`%${param}%`]);
+            res.json({
+                total : result.rowCount,
+                heroes : result.rows
+            });
+        } else {
+            const result = await pool.query('SELECT * FROM herois WHERE id = $1', [param]);
+            res.json({
+                total : result.rowCount,
+                heroes : result.rows
+            });
+        }
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.json({ error: error.message });
+    }
+}
