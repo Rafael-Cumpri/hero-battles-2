7c284bc873ef4805ecadfd05117f6e81c9d96273
Rafael Custodio Cumpri <rafaelcumpri.senai564@gmail.com>
2024-05-08 17:21:14 -0300

    feat : battleRoutes in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const heroesRoute = require('./routes/heroes.routes');
+const battleRoutes = require('./routes/battles.routes')
 
 const app = express();
 const port = process.env.PORT || 3000;  // Default to 3000 if PORT is not set
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3000;  // Default to 3000 if PORT is not set
 app.use(express.json());
 
 app.use('/', heroesRoute);
+app.use('/', battleRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}🧙🪄✨`);
