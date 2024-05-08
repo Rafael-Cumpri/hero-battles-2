60ff4ab2d86a3cb0768dd326b49581a5e3932b18
Rafael Custodio Cumpri <rafaelcumpri.senai564@gmail.com>
2024-05-08 17:12:09 -0300

    feat : dotenv Install

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 require('dotenv').config();
 const express = require('express');
-const bruxosRoutes = require('./routes/bruxosRoutes');
-const varinhasRoutes = require('./routes/varinhasRoutes');
+const heroesRoute = require('./routes/heroes.routes');
 
 const app = express();
 const port = process.env.PORT || 3000;  // Default to 3000 if PORT is not set
 
 app.use(express.json());
 
-app.use('/', heroes);
-app.use('/', battles);
+app.use('/', heroesRoute);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}🧙🪄✨`);
