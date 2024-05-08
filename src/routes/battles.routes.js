767a1661422cd23c1b47bf02c7350927f3dde0bb
Rafael Custodio Cumpri <rafaelcumpri.senai564@gmail.com>
2024-05-08 16:51:36 -0300

    feat : Battles Routes

---
+++ src/routes/battles.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const  = require('');
+
+router.post('/battles/:id1/:id2', );
+router.get('/battles/:param', );
+
+
+module.exports = router;
