e5f56179d3fee411ad5a783ca4d1aebcb2164a98
Rafael Custodio Cumpri <rafaelcumpri.senai564@gmail.com>
2024-05-08 17:08:29 -0300

    feat : functions on the route

--- src/routes/heroes.routes.js
+++ src/routes/heroes.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const  = require('');
+const heroesController = require('../controllers/heroes.controllers');
 
-router.post('/heroes', );
-router.get('/heroes', );
-router.get('/heroes/:param', );
-router.put('/heroes/:id', );
-router.delete('/heroes/:id', );
+router.post('/heroes', heroesController.createHero);
+router.get('/heroes', heroesController.getAllHeros);
+router.get('/heroes/:param', heroesController.getHeroByParam);
+router.put('/heroes/:id', heroesController.updateHero);
+router.delete('/heroes/:id', heroesController.deleteHero);
 
 module.exports = router;
