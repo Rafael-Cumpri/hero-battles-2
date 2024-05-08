95d3c517cf5a5c8c65b4a75c8f49116b3165d1a9
Rafael Custodio Cumpri <rafaelcumpri.senai564@gmail.com>
2024-05-08 16:51:26 -0300

    feat : Heroes Routes

---
+++ src/routes/heroes.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const  = require('');
+
+router.post('/heroes', );
+router.get('/heroes', );
+router.get('/heroes/:param', );
+router.put('/heroes/:id', );
+router.delete('/heroes/:id', );
+
+module.exports = router;
